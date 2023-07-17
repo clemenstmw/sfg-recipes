8b7e07a2b77983cc9c6b0b2b0e0bb5ba21221ddd
Clemens Thees (ext.) <Clemens.CT.Thees@partner.bmw.de>
2023-07-17 11:18:52 +0200

    INITIAL project setup commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sfg-recipes
+
 recipes app to practice web development
---
+++ src/test/java/de/maibornwolff/sfgrecipes/SfgRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.maibornwolff.sfgrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
