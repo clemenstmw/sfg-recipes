ec3dc8ab8aac9060af19a4284a10dffd62ec25e7
Clemens Thees (ext.) <Clemens.CT.Thees@partner.bmw.de>
2023-07-20 10:19:58 +0200

    ADD repository methods for category and UOM

--- src/main/java/de/maibornwolff/sfgrecipes/controllers/IndexController.java
+++ src/main/java/de/maibornwolff/sfgrecipes/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package de.maibornwolff.sfgrecipes.controllers;
 
+import de.maibornwolff.sfgrecipes.domain.Category;
+import de.maibornwolff.sfgrecipes.domain.UnitOfMeasure;
+import de.maibornwolff.sfgrecipes.repository.CategoryRepository;
+import de.maibornwolff.sfgrecipes.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> american = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> teaspoon = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + american.get().getId());
+        System.out.println("UOM Id is: " + teaspoon.get().getId());
+
         return "index";
     }
 }
